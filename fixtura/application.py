a63181df958b83bc18f6af5a7c4b733b4644c6fd
Екатерина <erusetskaya@gmail.com>
2020-04-24 00:26:50 +0300

    тесты добавления/удаления проекта

---
+++ fixtura/application.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from fixtura.session import SessionHelper
+from fixtura.project import ProjectHelper
+
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == 'Firefox':
+            self.wd = webdriver.Firefox()
+        elif browser == 'Chrome':
+            self.wd = webdriver.Chrome()
+        elif browser == 'ie':
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(3)
+        self.session = SessionHelper(self)
+        self.project = ProjectHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        if wd.current_url is not self.base_url:
+            wd.get(self.base_url)
+
+    def return_to_home_page(self):
+        wd = self.wd
+        if wd.current_url is not "http://localhost/addressbook/":
+            wd.find_element_by_link_text("home").click()
+
+    def destroy(self):
+        self.wd.quit()
